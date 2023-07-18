f030bdad7e5d8debee88b0bc30d97e8d500540ce
Vincent <vshan0816@gmail.com>
2023-07-18 16:25:58 -0400

    Work on board logic

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -1,9 +1,12 @@
 require_relative "piece"
-
+require "byebug"
 class Board
   def initialize
     @board = Array.new(8){Array.new(8)}
-    # iterater thr arr
+    # iterate through the board, at the positions to place nil
+    # iterate through the board, to place each piece
+    place_nil
+    place_piece
   end
 
   def [](pos)
@@ -17,10 +20,31 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-    if @board[start_pos]
-    @board[start_pos] = end_pos
-
+    if @board[end_pos].value != nil && @board[start_pos].value != nil && Board.check_valid?(end_pos)
+      debugger
+      @board[end_pos] = @board[start_pos]
+      @board[start_pos] = @null_piece
+    else
+      raise "cannot move to end position"
+    end
   end
 
+  def self.check_valid?(pos)
+    x, y = pos
+    (x >= 0 && x < 8) && (y >= 0 && y < 8)
+  end
+  
+  private
+  @null_piece = Piece.new
+  def place_nil
+    (2..5).each do |x|
+      (0..7).each do |y|
+        @board[x][y] = Piece.new
+      end
+    end
+  end
 
+  def place_piece
+    @board[0][0] = Piece.new(:B)
+  end
 end
