3701fe4611b5076c9288aabeac9944108fe7d950
elevenstand7 <elevenstand7@gmail.com>
2023-07-18 13:46:58 -0400

    create classes

---
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
@@ -0,0 +1,13 @@
+class Employee
+  def initialize(name, title, boss, salary=10000)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+
+end
+
+
+
---
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
@@ -0,0 +1,12 @@
+require_relative "employee"
+
+class Manager < Employee
+  def initialize(name,title,salary=10000, boss)
+    super
+    employees=[]
+
+
+  end
+
+
+end
