95542cd7dabc89503373b55e73808a889a5f3bc2
elevenstand7 <elevenstand7@gmail.com>
2023-07-18 16:41:27 -0400

    debug move_piece

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -1,6 +1,8 @@
 require_relative "piece"
 require "byebug"
 class Board
+
+
   def initialize
     @board = Array.new(8){Array.new(8)}
     # iterate through the board, at the positions to place nil
@@ -20,20 +22,21 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-    if @board[end_pos].value != nil && @board[start_pos].value != nil && Board.check_valid?(end_pos)
-      debugger
-      @board[end_pos] = @board[start_pos]
-      @board[start_pos] = @null_piece
-    else
-      raise "cannot move to end position"
-    end
+    # debugger
+    # if self[end_pos].value != nil && self[start_pos].value != nil && Board.check_valid?(end_pos)
+      # debugger
+      self[end_pos] = self[start_pos]
+      self[start_pos] = @null_piece
+    # else
+    #   raise "cannot move to end position"
+    # end
   end
 
   def self.check_valid?(pos)
     x, y = pos
     (x >= 0 && x < 8) && (y >= 0 && y < 8)
   end
-  
+
   private
   @null_piece = Piece.new
   def place_nil
--- practice-for-ch-ruby-chess-long-practice-main/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -1,4 +1,6 @@
 class Piece
+
+  attr_accessor :value
   def initialize(value=nil)
     @value = value
 
