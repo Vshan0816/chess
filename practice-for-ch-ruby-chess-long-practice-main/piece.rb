77bb570705adaf51367db007f20e23f4b1cc4ca1
Vincent <vshan0816@gmail.com>
2023-07-19 09:23:02 -0400

    Create classes for pieces

--- practice-for-ch-ruby-chess-long-practice-main/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -1,5 +1,16 @@
-class Piece
+require_relative "bishop"
+require_relative "king"
+require_relative "knight"
+require_relative "pawn"
+require_relative "queen"
+require_relative "rook"
+require_relative "null_piece"
 
+class Piece
+  #color ivar should hold a symbol, blackwhite
+  #when we initialize we need to set the symbol, board instance, position
+  #ivar for board
+  #ivar for position, starting position
   attr_accessor :value
   def initialize(value=nil)
     @value = value
